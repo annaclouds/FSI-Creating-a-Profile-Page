6cbbdb1fc480998e9db1d98f9856251859110a32
annaclouds <napha@ymail.com>
2022-06-13 01:47:02 -0700

    added comments to the starter code.

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-
+// 1. Inspect the starter code.
 let content = document.querySelector('.js-generated.content')
 
+// 2. Create an H1 element.
 let header = document.createElement('h1')
+
+// 3. Set the class attribute.
 header.setAttribute('class', 'dog-name')
-header.append('Rizzo')
-content.append(header)
+
+// 4. Append 'Rizzo' to it.
+header.append('Rizzo')          // adding the content 'Rizzo'
+
+// 5. Append it to the Content Container.
+content.append(header)          // appending the 'Rizzo' header to the content variable in step 1 = adding it to our DOM.
 
 let dogContent = document.createElement('div')
 dogContent.setAttribute('class', 'dog-content')
